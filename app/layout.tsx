bf50e4c6ff5806b1bcfe094446842066a3f40b79
Vihaan <vverma270705@gmail.com>
2025-01-19 14:02:29 +0530

    Added vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import VantaBg from "@/components/VantaBg";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <VantaBg>{children}</VantaBg>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
