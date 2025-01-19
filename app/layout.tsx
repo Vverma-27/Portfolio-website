b06e316d06063e5271e6397b7db91e3cd89c824a
Vihaan <vverma270705@gmail.com>
2025-01-19 14:00:55 +0530

    Added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import VantaBg from "@/components/VantaBg";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         <VantaBg>{children}</VantaBg>
+        <Analytics />
       </body>
     </html>
   );
